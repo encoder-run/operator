5d3e50f0f6cee549283547734efeba852b7417cf
Ark Pandey <arkpandey@Arks-MacBook-Pro.local>
2024-05-21 13:33:21 -0700

    fix:SerchEnter

--- frontend/console-ui/src/api/types.tsx
+++ frontend/console-ui/src/api/types.tsx
@@ -1,5 +1,4 @@
 import { gql } from '@apollo/client';
-import * as React from 'react';
 import * as Apollo from '@apollo/client';
 import * as ApolloReactComponents from '@apollo/client/react/components';
 export type Maybe<T> = T | null;
