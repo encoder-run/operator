f96d2dce415e619169f5a38a2f7d267cb5841d63
Ark Pandey <arkpandey@Arks-MacBook-Pro.local>
2024-05-23 08:55:55 -0700

    fix: ; at useEffect

--- frontend/console-ui/src/pages/pipeline-details/index.tsx
+++ frontend/console-ui/src/pages/pipeline-details/index.tsx
@@ -79,7 +79,7 @@ export default function PipelineDetailsPage() {
         }, 900000);
 
         return () => {
-            clearInterval(interval)
+            clearInterval(interval);
             clearTimeout(timeout);
         };
     }, [executionsRefetch]);
